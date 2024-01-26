96e5b9d29b298b1233d3d729c802bfee0e1290b0
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2024-01-27 00:07:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-function aveSpd(upTime, upSpd, downSpd) {
+function aveSpd(upTime, downTime, downSpd) {
   // your code here
+	 // uphill, down hill dist is same
+	
+	let dist = (Math.floor(downTime/60))*downSpd;
+	let upSpd = Math.floor(dist/upTime);
+
+	return (Math.floor((upSpd+downSpd)/2))
+	
 }
 
-// Do not change the code below
-const upTime = prompt("Enter upTime: ");
-const downTime = prompt("Enter downTime: ");
-const downSpd = prompt("Enter downSpd: ");
+//Do not change the code below
+const upTime = parseInt(prompt("Enter upTime: "));
+const downTime = parseInt(prompt("Enter downTime: "));
+const downSpd = parseInt(prompt("Enter downSpd: "));
 
-alert(aveSpd(upTime, downTime, downSpd));
+alert(aveSpd(upTime, downTime, downSpd)); 
